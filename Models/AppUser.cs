0e5199cd3f9d91fdce67d1a98b72b65d36821c04
Pham Quang Thanh - N1 <thanhpqce171732@fpt.edu.vn>
2025-03-02 18:09:51 +0700

    add default Project

---
+++ Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class AppUser:IdentityUser
+    {
+        public DateTime? CreateDate { get; set; }
+        public decimal tien { get; set; } = 0;
+        public string? Fullname { get; set; }
+        public DateTime lastAssces { get; set; } = DateTime.Now;
+    }
+}
---
+++ Models/DBContext/EasyFoodDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DBContext
+{
+    public class EasyFoodDbContext:IdentityDbContext<AppUser>
+    {
+ /*       public EasyFoodDbContext(DbContextOptions<EasyFoodDbContext> options) : base(options)
+        {
+        }
+*/
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+            // Bỏ tiền tố AspNet của các bảng: mặc định
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      => optionsBuilder.UseSqlServer("Server=DESKTOP-1E1A6I4;Database =EasyFood;uid=sa;pwd=Thanh;encrypt=true;trustServerCertificate=true;");
+    
+}
+}
