a32c07e9f0153209c3ff04dc58b4072b14fd9b0e
Trần Gia Huy <huytgcs170073@fpt.edu.vn>
2025-03-13 17:02:36 +0700

    add sellerAPI

---
+++ Repository/ViewModels/ReivewViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Models;
+
+namespace Repository.ViewModels
+{
+    public class ReivewViewModel
+    {
+        public Guid ID { get; set; }
+        public string? Cmt { get; set; }
+        public DateTime Datecmt { get; set; } = DateTime.Now;
+
+        public string? Relay { get; set; }
+        public DateTime DateRelay { get; set; } = DateTime.Now;
+        public bool Status { get; set; } = false;
+        public int Rating { get; set; } = 5;
+
+        public string UserID { get; set; }
+        [ForeignKey("Product")]
+        public Guid ProductID { get; set; }
+        public virtual Product Product { get; set; }
+        public AppUser AppUser { get; set; }
+        //public string OrderCode { get; set; }
+        //public string Username { get; set; }
+        //public string ProductName { get; set; }
+
+
+    }
+}
