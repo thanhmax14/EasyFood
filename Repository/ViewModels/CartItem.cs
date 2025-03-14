cb0cd1685c0c042b91323e7401e5231c020a9418
Pham Quang Thanh - N1 <thanhpqce171732@fpt.edu.vn>
2025-03-14 14:24:03 +0700

    add cart v1

---
+++ Repository/ViewModels/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.ViewModels
+{
+    public class CartItem
+    {
+        public Guid ProductID { get; set; }
+        public int quantity { get; set; } = 0;
+
+    }
+}
