f3eb70923ee8a109102d26d105a467aa6c71293c
Pham Quang Thanh - N1 <thanhpqce171732@fpt.edu.vn>
2025-03-24 06:09:58 +0700

    a

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -13,13 +13,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddOcelot();
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
